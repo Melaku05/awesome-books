727e49e399faa78a9eb5f45d9a6bd8511e78ef85
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-03-21 12:32:10 +0300

    Create main javascript functionality.
    - Create books object.
    - Create Add new book function;
    - Create loadContent function to load books on browser loadings.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-class Library {
-  constructor() {}
-  add(title, author) {}
-  remove(author) {}
+const booksList = document.querySelector('.books__list');
+const addBtn = document.querySelector('#submit');
+const book = document.querySelector('#book');
+const author = document.querySelector('#author');
+
+let BooksObject = [{}];
+
+function loadContent() {
+  booksList.innerHTML = '';
+  console.log(JSON.stringify(BooksObject));
+  BooksObject.forEach(function (obj) {
+    booksList.innerHTML += `<div>
+                    <h4>${obj.title}</h4>
+                    <h3>${obj.author}</h3>
+                    <button>Remove</button>
+                </div>`;
+  });
 }
+
+function addNewBook() {
+  BooksObject.push({ title: book.value, author: author.value });
+  loadContent();
+}
+
+function removeBook() {}
+
+function updateLocalStorage() {}
+
+addBtn.addEventListener('click', addNewBook);
+
+document.addEventListener('DOMContentLoaded', loadContent);
