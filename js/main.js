e49eb2e773c0082a838d051684276f4740a76b4c
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-03-21 16:37:32 +0300

    skipe eslint for unused variable, function and classes

--- js/main.js
+++ js/main.js
@@ -30,13 +30,16 @@ function addNewBook() {
   updateLocalStorage();
   loadContent();
 }
+/* eslint-disable */
 
 function removeBook(element) {
   const id = element.parentElement.className;
   element.remove();
-  BooksObject.splice(id, 1);
+  BooksObject.splice(parseInt(id), 1);
   updateLocalStorage();
 }
+
+/* eslint-enable */
 
 function checkLocalStorage() {
   if (JSON.parse(localStorage.getItem(KEY)) != null) {
